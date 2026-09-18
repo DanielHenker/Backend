526701dad86383b83392852c0eb73c22808e208a
DanielHenker <danielfel.dfsh@gmail.com>
2026-09-17 21:50:38 -0500

    feat: add CORS support to the server and update dependencies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 1. importar las dependencias necesarias
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { connectionMongo } from './src/config/dataBase.js'
 import { userRouter } from './src/routes/users.routes.js'
 import { productsRouter } from './src/routes/products.routes.js'
@@ -10,8 +11,11 @@ import { appointmentsRouter } from './src/routes/appointments.routes.js'
 const app = express() //llamar a express para crear la aplicación
 dotenv.config() // permite llamar las variables de entorno de .env
 let port = process.env.PORT;
-connectionMongo() //llamar a la función para conectar a la base de datos
-app.use(express.json()) // permite que el servidor entienda los datos en formato JSON
+connectionMongo(); //llamar a la función para conectar a la base de datos
+
+app.use(cors()); // permite que el servidor acepte solicitudes de otros dominios
+
+app.use(express.json()); // permite que el servidor entienda los datos en formato JSON
 
 
 // 3. crear las rutas necesarias para el servidor
